d1036708d74c53959774bd22863ffa823c5eee84
este <esteban.h0207@gmail.com>
2026-01-25 20:43:32 +0100

    add logo png and novbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Links from "./Links";
 import Link from "next/link";
+import Image from "next/image";
 import { motion } from "framer-motion"
 
 export default function Navbar() {
@@ -11,7 +12,9 @@ export default function Navbar() {
             transition={{ duration: 0.5 }}
             className="w-420 flex justify-between items-center py-4 px-8 bg-white/50 shadow-md fixed top-2 left-20 z-50 rounded-full"
         >
-            <Link href="/" className="text-2xl font-bold">Philips</Link>
+            <Link href="/" className="text-2xl font-bold">
+                <Image src="/logo.png" alt="Logo" width={40} height={40} />
+            </Link>
             <div className="flex space-x-6 text-xl">
                 <Links href="/" name="Home" />
                 <Links href="/about" name="About" />
