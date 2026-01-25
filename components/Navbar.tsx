37bc07010033dd377b3939d5c5e8673c2bb5cb8c
este <esteban.h0207@gmail.com>
2026-01-25 21:43:01 +0100

    changement designe de la navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,16 +10,22 @@ export default function Navbar() {
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5 }}
-            className="w-420 flex justify-between items-center py-4 px-8 bg-white/50 shadow-md fixed top-2 left-20 z-50 rounded-full"
+            className="w-420 grid grid-cols-3 items-center py-0 px-8 bg-white/50 shadow-md fixed top-2 left-20 z-50 rounded-xl"
         >
             <Link href="/" className="text-2xl font-bold">
-                <Image src="/logo.png" alt="Logo" width={40} height={40} />
+                {/* <Image src="/logo.png" alt="Logo" width={40} height={40} /> */}
+                Philips
             </Link>
-            <div className="flex space-x-6 text-xl">
+            <div className="flex justify-center space-x-6 text-white text-xl bg-black ml-10 p-3 rounded-xl">
                 <Links href="/" name="Home" />
                 <Links href="/about" name="About" />
                 <Links href="/Products" name="Products" />
                 <Links href="/contact" name="Contact" />
+            </div>
+            <div className="flex justify-end items-center">
+                <Link href="/cart" className="ml-4 text-xl font-semibold">
+                    Cart
+                </Link>
             </div>
         </motion.nav>
     );
