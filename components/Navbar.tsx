62e214893d442c0959901500622f769ed357efd8
este <esteban.h0207@gmail.com>
2026-01-25 21:59:20 +0100

    add pins components & fix narvbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@
 import Links from "./Links";
 import Link from "next/link";
 import Image from "next/image";
+import Pins from "./Pins";
+import { FaShoppingCart } from "react-icons/fa";
 import { motion } from "framer-motion"
 
 export default function Navbar() {
@@ -9,22 +11,27 @@ export default function Navbar() {
         <motion.nav 
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.7 }}
             className="w-420 grid grid-cols-3 items-center py-0 px-8 bg-white/50 shadow-md fixed top-2 left-20 z-50 rounded-xl"
         >
-            <Link href="/" className="text-2xl font-bold">
-                {/* <Image src="/logo.png" alt="Logo" width={40} height={40} /> */}
-                Philips
-            </Link>
-            <div className="flex justify-center space-x-6 text-white text-xl bg-black ml-10 p-3 rounded-xl">
-                <Links href="/" name="Home" />
-                <Links href="/about" name="About" />
-                <Links href="/Products" name="Products" />
-                <Links href="/contact" name="Contact" />
+            <div className="flex">
+                <Link href="/" className="text-2xl font-bold">
+                    {/* <Image src="/logo.png" alt="Logo" width={40} height={40} /> */}
+                    Philips
+                </Link>
+            </div>
+            <div className="flex justify-center space-x-6 text-xl bg-black ml-10 p-3 rounded-xl">
+                <ul className="flex gap-5 items-center text-white list-disc pl-5">
+                    <Links href="/" name="Home" />
+                    <Links href="/about" name="About" />
+                    <Links href="/Products" name="Products" />
+                    <Links href="/contact" name="Contact" />
+                </ul>
             </div>
             <div className="flex justify-end items-center">
                 <Link href="/cart" className="ml-4 text-xl font-semibold">
-                    Cart
+                    {/* Cart */}
+                <Pins icon={<FaShoppingCart />} />
                 </Link>
             </div>
         </motion.nav>
