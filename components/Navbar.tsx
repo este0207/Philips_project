793c76348170e8f0f05c6d0524024787121d9162
este <esteban.h0207@gmail.com>
2026-01-26 18:31:21 +0100

    add color change in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,14 @@ import Pins from "./Pins";
 import { FaShoppingCart } from "react-icons/fa";
 import { MdLanguage } from "react-icons/md";
 import { motion } from "framer-motion"
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const pathname = usePathname();
+
+    const isProductPage = pathname?.toLowerCase().includes("/products");
+
+    const textColor = isProductPage ? "text-black" : "text-white";
     return (
         <nav className="w-full flex items-center py-4 px-8 bg-transparent justify-between fixed top-10 left-0 z-50">
             <motion.div 
@@ -16,13 +22,13 @@ export default function Navbar() {
             transition={{ duration: 0.7 }}
             className="flex bg-white/10 backdrop-blur-md rounded-full justify-between items-center px-6 py-3 shadow-lg gap-5">
                 <div className="flex">
-                    <Link href="/" className="flex text-white text-2xl font-bold">
+                    <Link href="/" className={`flex ${textColor} text-2xl font-bold`}>
                         <Image src="/logoN.png" alt="Logo" width={40} height={40} />
                         Philips
                     </Link>
                 </div>
                 <div className="flex justify-center space-x-10 text-xl ">
-                    <ul className="flex gap-10 items-center text-white list-none pl-5">
+                    <ul className={`flex gap-10 items-center ${textColor} list-none pl-5`}>
                         <li><Links href="/" name="HOME" /></li>
                         <li><Links href="/about" name="ABOUT" /></li>
                         <li><Links href="/Products" name="PRODUCTS" /></li>
@@ -34,8 +40,8 @@ export default function Navbar() {
             initial={{ x: 100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.7 }}
-            className="flex gap-2">
-                <Link href="/cart" className="text-white">
+            className={`flex gap-2 ${textColor}`}>
+                <Link href="/cart">
                     <Pins icon={<FaShoppingCart />} text="CART" />
                 </Link>
                     <Pins icon={<MdLanguage />} text="" />
--- components/Pins.tsx
+++ components/Pins.tsx
@@ -1,6 +1,6 @@
 export default function Pins( { icon, text }: { icon: React.ReactNode; text: string } ) {
     return (
-        <button className="bg-black/10 text-white flex justify-center text-xl items-center gap-2 p-3 rounded-full hover:bg-black/50 hover:cursor-pointer transition">
+        <button className="bg-black/10  flex justify-center text-xl items-center gap-2 p-3 rounded-full hover:bg-black/50 hover:cursor-pointer transition">
             {icon}
             {text}
         </button>
