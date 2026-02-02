ea6361d4b5f8148a0369fcbf68243af9671da156
este <esteban.h0207@gmail.com>
2026-02-03 00:21:38 +0100

    add

--- components/ui/ProductInfoContainer.tsx
+++ components/ui/ProductInfoContainer.tsx
@@ -26,21 +26,21 @@ export default function ProductInfoContainer() {
     }
 
     return (
-        <div className="p-8 bg-white/70 backdrop-blur-2xl absolute top-0 left-0 w-full h-screen pt-35 flex flex-col items-center">
-            <ReturnBtn />
+        <div className="p-8 bg-white/40 backdrop-blur-2xl w-full h-screen pt-35 flex flex-col items-center">
+            {/* <ReturnBtn /> */}
             <motion.div 
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, ease: "easeOut" }}
-            className="max-w-3xl w-full bg-white p-6 rounded-lg shadow-lg grid grid-cols-2 items-center"
+            className="max-w-3xl w-full bg-white p-6 rounded-lg shadow-lg grid grid-cols-2 items-center h-screen"
             >
                 <div className="mb-4 flex justify-center items-center w-full">
-                    <Image src={product.product_image || '/placeholder.png'} alt="product_Image" width={200} height={200} className="rounded-xl object-cover" />
+                    <Image src={product.image || '/placeholder.png'} alt="product_Image" width={200} height={200} className="rounded-xl object-cover" />
                 </div>
                 <div className="pl-6 gap-4 flex flex-col">
-                    <h1 className="text-3xl font-bold mb-4">{product.product_name}</h1>
-                    <p className="text-xl font-semibold mb-2">Prix : {product.product_price}</p>
-                    <p className="text-gray-700">{product.product_desc}</p>
+                    <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+                    <p className="text-xl font-semibold mb-2">Prix : {product.price}</p>
+                    <p className="text-gray-700">{product.desc}</p>
                     <AddCartBtn />
                 </div>
             </motion.div>
