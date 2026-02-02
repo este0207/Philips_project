3761c1fb62f440aaeae1e714f39a4a0282060524
este <esteban.h0207@gmail.com>
2026-02-03 00:28:12 +0100

    add cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,7 @@
+export default function CartPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-white">This is the Cart Page</h1>
+    </div>
+  );
+}
