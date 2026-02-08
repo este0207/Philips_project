e7da3fad9c1bddfcb1355cd3a47e0df0998f150e
este <esteban.h0207@gmail.com>
2026-02-08 18:50:04 +0100

    add cart and fix about

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,26 @@
 export default function CartPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-white">This is the Cart Page</h1>
-    </div>
+    <main className="p-8 bg-white/90 top-0 left-0 w-full min-h-screen">
+      <div className="pt-25 h-full flex flex-col items-center">
+        <h1 className="text-3xl font-bold mb-6 text-black">Votre panier</h1>
+
+        <div className="w-full max-w-5xl bg-white/10 backdrop-blur-md rounded-3xl shadow-lg p-6 md:p-8 border border-white/20">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-4">
+
+
+            </div>
+
+            <div className="flex flex-col md:flex-row items-center justify-between gap-6 bg-white/90 rounded-2xl p-6 shadow">
+              <div className="flex flex-col gap-2">
+                <span className="text-sm text-neutral-500">Sous-total</span>
+                <span className="text-2xl font-bold text-neutral-900">0€</span>
+                <span className="text-sm text-neutral-500">TVA et frais inclus</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
